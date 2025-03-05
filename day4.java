0510bab67ec78e652e18d9f0443db7d9a0deb088
Nishita-16 <nishuverma105514@gmail.com>
2025-03-05 17:20:54 +0530

    Update day4.java

--- day4.java
+++ day4.java
@@ -1,3 +1,13 @@
+//que 1
+//problem link:
+class Solution {
+    public int distinctIntegers(int n) {
+        return n>1?n-1:1;
+    }
+}
+TC:O(1)
+SC:O(1)
+approach:as n%i must be =1 so it will the number before it and so on for the same number
 // que 2
 //problem link:https://www.geeksforgeeks.org/problems/chocolate-distribution-problem3825/1
 class Solution {
