fde233c3d8a352abd83534fe7384e23390b2be3b
Nishita-16 <nishuverma105514@gmail.com>
2025-03-05 22:03:56 +0530

    Update day3.java

--- day3.java
+++ day3.java
@@ -19,3 +19,27 @@ class Solution {
 }
 //sc:O(logn)
 //tc:O(logn)
+// que 2
+// problem link:https://leetcode.com/problems/flipping-an-image/
+class Solution {
+    public void flip(int arr[]){
+        for(int i=0;i<arr.length/2;i++){
+            int temp=arr[i];
+            arr[i]=arr[arr.length-1-i];
+            arr[arr.length-1-i]=temp;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==1)arr[i]=0;
+            else arr[i]=1;
+        }
+    }
+    public int[][] flipAndInvertImage(int[][] image) {
+        int i;
+        for(i=0;i<image.length;i++){
+            flip(image[i]);
+        }
+        return image;
+    }
+}
+//tc:O(n*m)
+//sc:O(1)
