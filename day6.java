9efd39555c11ca98ad62469f8cfb9197ba37729e
Nishita-16 <nishuverma105514@gmail.com>
2025-03-10 19:21:08 +0530

    Update day6.java

--- day6.java
+++ day6.java
@@ -1,3 +1,26 @@
 leetcode potd 
 date:07/03/2025
 problem link:https://leetcode.com/problems/closest-prime-numbers-in-range/solutions/?envType=daily-question&envId=2025-03-07
+que1
+probltm link:https://www.geeksforgeeks.org/problems/stock-span-problem-1587115621/1
+class Solution {
+    public ArrayList<Integer> calculateSpan(int[] arr) {
+        // write code here
+        Stack <Integer> stk=new Stack<>();
+        ArrayList <Integer> list=new ArrayList <>();
+        int i,j;
+        for(i=0;i<arr.length;i++){
+            while(!stk.isEmpty() && arr[stk.peek()]<=arr[i]){
+                stk.pop();
+            }
+            if(stk.isEmpty()){
+                list.add(i+1);
+            }
+            else{
+                list.add(i-stk.peek());
+            }
+            stk.push(i);
+        }
+        return list;
+    }
+}
