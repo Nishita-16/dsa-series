c8f9c85457be98b39afed9dea87a2b5557774def
Nishita-16 <nishuverma105514@gmail.com>
2025-03-10 19:35:48 +0530

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -1,3 +1,21 @@
+//probem 1
+//link:https://leetcode.com/problems/ugly-number/
+class Solution {
+    public boolean isUgly(int n) {
+        int i;
+        if(n==0)return false;
+        while(n%2==0){
+            n/=2;
+        }
+        while(n%3==0){
+            n/=3;
+        }
+        while(n%5==0){
+            n/=5;
+        }
+        return (n==1);
+    }
+}
 // problem2:-
 // https://www.geeksforgeeks.org/problems/reverse-bits3556/1?page=1&category=Mathematical&difficulty=Easy&sortBy=submissions
 class Solution {
