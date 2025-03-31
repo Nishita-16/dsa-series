a8259ec38e8437a71ba6db73fd9828bdd6b0e117
Nishita-16 <nishuverma105514@gmail.com>
2025-03-31 12:19:02 +0530

    Create day28.java

---
+++ day28.java
@@ -0,0 +1,26 @@
+problwm:https://leetcode.com/problems/middle-of-the-linked-list/description/
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
+problem:https://leetcode.com/problems/linked-list-cycle/description/
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head,fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
